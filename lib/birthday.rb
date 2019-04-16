bdb2455fc4ddeff5e83e010a820dba2a0047dc85
Billy Joseph <ingbillyjoseph@gmail.com>
2019-04-16 12:50:55 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,7 +9,7 @@ def happy_birthday(birthday_kids)
   birthday_kids = {"Timmy" => 9, "Sarah" => 6, "Amanda" => 27}
   
   birthday_kids.each do |key, value|
-    puts "Happy Birthday #{key}! You are now #{value} years old!:"
+    puts "Happy Birthday #{key}! You are now #{value} years old!"
   end 
 end
 
